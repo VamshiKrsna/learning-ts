5002efd81f262789cf0922ac85716ac74227458d
Vamshi Krishna S <107762231+VamshiKrsna@users.noreply.github.com>
2024-12-22 23:48:15 +0530

    added todo app

---
+++ DataTypes.js
@@ -0,0 +1,13 @@
+let names = "JonSnow"; //String
+let number = 42; //Number
+let boolean = true;
+let undef;
+let nullType = null;
+let symType = Symbol("unique");
+
+console.log(typeof names)
+console.log(typeof number)
+console.log(typeof boolean)
+console.log(typeof undef)
+console.log(typeof nullType)
+console.log(typeof symType)
