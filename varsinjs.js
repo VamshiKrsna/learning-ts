2c5f136551c8cbd1cb8cb9f5a2639f7568ec4f69
Vamshi Krishna S <107762231+VamshiKrsna@users.noreply.github.com>
2024-12-22 23:25:11 +0530

    Add files via upload

---
+++ varsinjs.js
@@ -0,0 +1,12 @@
+// Variables and types
+
+var string = "Jon Snow";
+console.log(string);
+
+let age = 27;
+console.log(age);
+
+const pi = 3.14;
+console.log(pi);
+//pi = 4; //error
+// console.log(pi);
